85d8859d2de572b2c0afe9606a893379fb4b4f52
JianjunHuang <demon903732218@gmail.com>
2020-01-20 11:25:00 +0800

    update

--- app/src/main/java/com/example/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/example/cameraxdemo/MainActivity.kt
@@ -2,19 +2,27 @@ package com.example.cameraxdemo
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
-import android.util.Size
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
 import android.view.TextureView
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
+import java.nio.ByteBuffer
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         // Add this at the end of onCreate function
 
         viewFinder = findViewById(R.id.view_finder)
+        captureBtn = findViewById(R.id.ib_capture)
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -44,14 +53,54 @@ class MainActivity : AppCompatActivity() {
 
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var viewFinder: TextureView
+    private lateinit var captureBtn: ImageButton
+    private val cameraControl by lazy { CameraX.getCameraControl(CameraX.LensFacing.BACK) }
+
+    private val orientationEventListener by lazy {
+        object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN) {
+                    return
+                }
+
+                captureBtn.animate().rotation(
+                    if (orientation > 350 || orientation < 10) { //0度
+                        -0f
+                    } else if (orientation in 81..99) { //90度
+                        -90f
+                    } else if (orientation in 171..189) { //180度
+                        -180f
+                    } else if (orientation in 261..279) { //270度
+                        -270f
+                    } else {
+                        -0f
+                    }
+                ).start()
+
+            }
+        }
+    }
+
+    private fun aspectRatio(width: Int, height: Int): AspectRatio {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
 
     private fun startCamera() {
+        val metrics = DisplayMetrics().also { viewFinder.display.getRealMetrics(it) }
+        Log.d(TAG, "Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
         val previewConfig =
             PreviewConfig.Builder()
-                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                .setTargetAspectRatio(screenAspectRatio)
                 .setLensFacing(CameraX.LensFacing.BACK)
+                .setTargetRotation(viewFinder.display.rotation)
                 .build()
-        val preview = Preview(previewConfig)
+        val preview =AutoFitPreviewBuilder.build(previewConfig, viewFinder)
 
         // Every time the viewfinder is updated, recompute layout
         preview.setOnPreviewOutputUpdateListener { previewOutput ->
@@ -61,15 +110,97 @@ class MainActivity : AppCompatActivity() {
             parent.addView(viewFinder, 0)
 
             viewFinder.surfaceTexture = previewOutput.surfaceTexture
+
+            updateTransform()
+        }
+        // Add this before CameraX.bindToLifecycle
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                // We don't set a resolution for image capture; instead, we
+                // select a capture mode which will infer the appropriate
+                // resolution based on aspect ration and requested mode
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+            }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        captureBtn.setOnClickListener {
+            val file = File(
+                externalMediaDirs.first(),
+                "${System.currentTimeMillis()}.jpg"
+            )
+
+            imageCapture.takePicture(file, executor,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        imageCaptureError: ImageCapture.ImageCaptureError,
+                        message: String,
+                        exc: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        Log.e("CameraDemo", msg, exc)
+                        viewFinder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        Log.d("CameraDemo", msg)
+                        viewFinder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+        }
+
+
+        // Add this before CameraX.bindToLifecycle
+
+        // Setup image analysis pipeline that computes average pixel luminance
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE
+            )
+        }.build()
+
+        // Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, LuminosityAnalyzer())
         }
 
         // Bind use cases to lifecycle
         // If Android Studio complains about "this" being not a LifecycleOwner
         // try rebuilding the project or updating the appcompat dependency to
         // version 1.1.0 or higher.
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, imageCapture, analyzerUseCase)
     }
 
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        Log.i(TAG, "preview rotation degrees = $rotationDegrees")
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
+    }
 
     /**
      * Process result from permission request dialog box, has the request
@@ -106,5 +237,55 @@ class MainActivity : AppCompatActivity() {
         // request. Where an app has multiple context for requesting permission,
         // this can help differentiate the different contexts.
         private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+        private const val TAG = "CameraDemo"
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        orientationEventListener.enable()
+    }
+
+    override fun onPause() {
+        super.onPause()
+//        orientationEventListener.disable()
+    }
+
+    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        /**
+         * Helper extension function used to extract a byte array from an
+         * image plane buffer
+         */
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+            val currentTimestamp = System.currentTimeMillis()
+            // Calculate the average luma no more often than every second
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                TimeUnit.SECONDS.toMillis(1)
+            ) {
+                // Since format in ImageAnalysis is YUV, image.planes[0]
+                // contains the Y (luminance) plane
+                val buffer = image.planes[0].buffer
+                // Extract image data from callback object
+                val data = buffer.toByteArray()
+                // Convert the data into an array of pixel values
+                val pixels = data.map { it.toInt() and 0xFF }
+                // Compute average luminance for the image
+                val luma = pixels.average()
+                // Log the new luma value
+                Log.d("CameraXApp", "Average luminosity: $luma")
+                // Update timestamp of last analyzed frame
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+        }
     }
 }
